16084451a8156ecd0081ec77b881d43be4de4228
Ion Craciunescu <icraciunescu@hotmail.com>
2018-11-17 12:40:21 +0200

    test

---
+++ Color.java
@@ -0,0 +1,27 @@
+public class Color {
+
+    private String typeColor;
+    private int priceColor;
+
+    public Color(String typeColor, int priceColor) {
+        this.typeColor = typeColor;
+        this.priceColor = priceColor;
+    }
+
+    public String getTypeColor() {
+        return typeColor;
+    }
+
+    public void setTypeColor(String typeColor) {
+        this.typeColor = typeColor;
+    }
+
+    public int getPriceColor() {
+        return priceColor;
+    }
+
+    public void setPriceColor(int priceColor) {
+        this.priceColor = priceColor;
+    }
+
+}
---
+++ FuelConsumption.java
@@ -0,0 +1,16 @@
+public class FuelConsumption {
+
+    private double typeFuelConsumption;
+
+    public FuelConsumption(double typeFuelConsumption) {
+        this.typeFuelConsumption = typeFuelConsumption;
+    }
+
+    public double getTypeFuelConsumption() {
+        return typeFuelConsumption;
+    }
+
+    public void setTypeFuelConsumption(double typeFuelConsumption) {
+        this.typeFuelConsumption = typeFuelConsumption;
+    }
+}
